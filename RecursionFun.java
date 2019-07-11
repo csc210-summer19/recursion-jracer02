92a1b2dccec59e32cd9051ff977fb58ee3d55579
jrace <jrace@DESKTOP-8U2ORN7>
2019-07-10 23:52:54 -0700

    .

--- RecursionFun.java
+++ RecursionFun.java
@@ -1,12 +1,12 @@
 /**
- * Complete the seven methods methods in this file using recursion, no loops. 
+ * Complete the seven methods methods in this file using recursion, no loops.
  * Also complete these three methods inside LinkedList<E>:
- *   get(int)
- *   removeAll(E)
- *   duplicateAll(E)
- *   
+ * get(int)
+ * removeAll(E)
+ * duplicateAll(E)
+ * 
  * Also complete one method in ObstacleCourse that uses recursive backtracking.
- *   findExit(int, int)
+ * findExit(int, int)
  * 
  * Note: I believe the given unit test tests all methods. We will
  * be using this same for for correctness of code.
@@ -17,69 +17,139 @@
  */
 public class RecursionFun {
 
-  // Complete recursive method combinations that returns from n choose k.
-  // This method is described in 17_SimpleRecursion.pptx.
-  public int combinations(int n, int k) {
-    return Integer.MIN_VALUE;
-  }
+	// Complete recursive method combinations that returns from n choose k.
+	// This method is described in 17_SimpleRecursion.pptx.
+	public int combinations(int n, int k) {
+		if (n < k) {
+			return 0;
+		} else if (k == 1) {
+			return n;
+		} else if (n == k) {
+			return 1;
+		} else if ((n > k) && (k > 1)) {
+			return combinations(n - 1, k - 1) + combinations(n - 1, k);
+		}
+		return 0;
+	}
 
- 
-  // Complete recursive method intWithCommas that returns the argument as a String
-  // with commas in the correct places.
-  //
-  // intWithCommas(999) returns "999"
-  // intWithCommas(1234) returns "1,234"
-  // intWithCommas(1007) returns "1,007"
-  // intWithCommas(1023004567) returns "1,023,004,567"
-  //
-  // Precondition: n >= 0
-  public String intWithCommas(int n) {
-    // TODO: Implement this method using recursion. Do not use a loop
-    return "Under construction";
-  }
+	// Complete recursive method intWithCommas that returns the argument as a
+	// String
+	// with commas in the correct places.
+	//
+	// intWithCommas(999) returns "999"
+	// intWithCommas(1234) returns "1,234"
+	// intWithCommas(1007) returns "1,007"
+	// intWithCommas(1023004567) returns "1,023,004,567"
+	//
+	// Precondition: n >= 0
+	public String intWithCommas(int n) {
+		return intWithCommas(String.valueOf(n));
+	}
 
-  // Write recursive method reverseArray that reverses the array elements in a
-  // filled array of ints. Use recursion; do not use a loop. The following
-  // assertions must pass:
-  //
-  // int[] a = { 2, 4, 6 };
-  // rf.reverseArray(a);
-  // assertEquals(6, a[0]);
-  // assertEquals(4, a[1]);
-  // assertEquals(2, a[2]);
-  //
-  // Precondition: x.length > 0
-  public void reverseArray(int[] x) {
-    // You need a private helper that needs additional arguments.
-    // like x and x.length to keep track of array the indexes
-    // with no loop. Here is an example with the private helper
-    // immediately below.
-    reverseArray(x, 0, x.length);
-  }
+	private String intWithCommas(String str) {
+		if (str.length() <= 3) {
+			return str;
+		} else {
+			int breakPt = str.length() - 3;
+			return intWithCommas(str.substring(0, breakPt)) + ","
+					+ str.substring(breakPt);
+		}
+	}
 
-  private void reverseArray(int[] x, int index, int len) {
-    // TODO: Complete this method with a recursive algorithm. 
-    // Do NOT use a loop.
-  }
+	// Write recursive method reverseArray that reverses the array elements in a
+	// filled array of ints. Use recursion; do not use a loop. The following
+	// assertions must pass:
+	//
+	// int[] a = { 2, 4, 6 };
+	// rf.reverseArray(a);
+	// assertEquals(6, a[0]);
+	// assertEquals(4, a[1]);
+	// assertEquals(2, a[2]);
+	//
+	// Precondition: x.length > 0
+	public void reverseArray(int[] x) {
+		// You need a private helper that needs additional arguments.
+		// like x and x.length to keep track of array the indexes
+		// with no loop. Here is an example with the private helper
+		// immediately below.
+		reverseArray(x, 0, x.length);
+	}
 
-  // Write recursive method arrayRange that returns the maximum
-  // integer minus the minimum integer in the filled array of 
-  // integers, Use recursion; do not use a loop. 
-  // Precondition: a.length > 0
-  public int arrayRange(int[] a) {
-    return Integer.MIN_VALUE;
-  }
+	private void reverseArray(int[] x, int index, int len) {
+		// Even Length
+		// { 0, 1, 2, 3 }
+		if (index >= (len / 2)) {
+			return;
+		} else {
+			// not past midpoint
+			int tmp = x[index];
+			x[index] = x[len - 1 - index];
+			x[len - 1 - index] = tmp;
+			reverseArray(x, index + 1, len);
+		}
+	}
 
-  // Return true if nums has all int elements in ascending order.
-  // If not isSorted, return false.
-  public boolean isSorted(int[] nums) {
-    // Need to send down 0 to keep track of the index
-    return ! true;
-  }
+	// Write recursive method arrayRange that returns the maximum
+	// integer minus the minimum integer in the filled array of
+	// integers, Use recursion; do not use a loop.
+	// Precondition: a.length > 0
+	public int arrayRange(int[] a) {
 
-  // Complete method found to return true if search is found in strs.
-  // If not found, return false. Use equals, not ==.
-  public boolean found(String search, String[] strs) {
-     return ! true;
-  }
+		int min = findMin(a, 0);
+		int max = findMax(a, 0);
+
+		return max - min;
+	}
+
+	private int findMax(int[] a, int index) {
+		if (index < a.length) {
+			return Math.max(a[index], findMax(a, index + 1));
+		} else {
+			return a[0];
+		}
+	}
+
+	private int findMin(int[] a, int index) {
+		if (index < a.length) {
+			return Math.min(a[index], findMin(a, index + 1));
+		} else {
+			return a[0];
+		}
+	}
+
+	// Return true if nums has all int elements in ascending order.
+	// If not isSorted, return false.
+	public boolean isSorted(int[] nums) {
+		// Need to send down 0 to keep track of the index
+		return isSorted(nums, 0);
+	}
+
+	private boolean isSorted(int[] nums, int index) {
+		if (index < nums.length - 1) {
+			if (nums[index] > nums[index + 1]) {
+				return false;
+			} else {
+				return isSorted(nums, index + 1);
+			}
+		}
+		return true;
+	}
+
+	// Complete method found to return true if search is found in strs.
+	// If not found, return false. Use equals, not ==.
+	public boolean found(String search, String[] strs) {
+		return found(search, strs, 0);
+	}
+
+	private boolean found(String search, String[] strs, int index) {
+		if (index < strs.length) {
+			if (strs[index].equals(search)) {
+				return true;
+			} else {
+				return found(search, strs, index + 1);
+			}
+		} else {
+			return false;
+		}
+	}
 }
